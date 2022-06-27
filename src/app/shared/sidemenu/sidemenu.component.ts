61200130c3b321626662b9753982e1ce8f38c24b
unknown <whernandez@grupogd.com.sv>
2022-06-27 06:04:26 -0600

    Componentes y configuracion de lazy load

---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  texto: string;
+  ruta: string;
+}
+
+@Component({
+  selector: 'app-sidemenu',
+  templateUrl: './sidemenu.component.html',
+  styles: [
+  ]
+})
+export class SidemenuComponent {
+
+  templateMenu: MenuItem[] = [
+    {
+      texto: 'Basicos',
+      ruta: './template/basicos'
+    },
+    {
+      texto: 'Dinamicos',
+      ruta: './template/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: './template/switches'
+    }
+  ]
+
+  reactiveMenu: MenuItem[] = [
+    {
+      texto: 'Basicos',
+      ruta: './reactive/basicos'
+    },
+    {
+      texto: 'Dinamicos',
+      ruta: './reactive/dinamicos'
+    },
+    {
+      texto: 'Switches',
+      ruta: './reactive/switches'
+    }
+  ]
+
+}
